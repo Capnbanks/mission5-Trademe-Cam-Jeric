43fd473be138d06ad41bcea1a86e09520d93ee23
Cameron Banks <cam.banks41@gmail.com>
2024-03-31 15:10:12 +1300

    product Subpage sidebar finished

--- trade-me/src/pages/product-subpage/ProductSubpage.jsx
+++ trade-me/src/pages/product-subpage/ProductSubpage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import TrademeHeader from './components/TrademeHeader/TrademeHeader'
 import TrademeFooter from './components/TrademeFooter/TrademeFooter'
+import ProductSubPageMain from './components/ProductSubPageMainContent/ProductSubPageMain'
+import styles from './ProductSubpage.module.css'
 
 export default function ProductSubpage() {
   return (
-    <div>
+    <div className={styles.mainContainer}>
         <TrademeHeader/>
-        
-        ProductSubpage Main content
+        <ProductSubPageMain/>
+        {/* ProductS ubpage Main content */}
         <TrademeFooter/>
         </div>
   )
